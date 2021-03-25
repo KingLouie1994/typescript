f8a25d08b7167b8164f0ea7794a061e13a9ad73b
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-25 17:29:55 +0100

    basics with types string, number, boolean

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log("All TS code goes in here!");
+const add = (
+  num1: number,
+  num2: number,
+  showResult: boolean,
+  phrase: string
+) => {
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+};
+
+const number1 = 7;
+const number2 = 28;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
