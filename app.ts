1b34458197ab0cbba3d346922cb876617ca3ee82
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-25 18:14:59 +0100

    basics with types tuples, enums

--- app.ts
+++ app.ts
@@ -1,11 +1,34 @@
 // const person: {
 //   name: string;
 //   age: number;
-// }
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Luis",
+//   age: 26,
+//   hobbies: ["Coding", "Golf"],
+//   role: [2, "author"],
+// };
+
+// Does not work because of type
+// person.role[1] = 10;
+// person.role = [];
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "Luis",
   age: 26,
   hobbies: ["Coding", "Golf"],
+  role: Role.ADMIN,
 };
 
 let favoriteActivities: string[];
@@ -15,5 +38,9 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
-//   console.log(hobby.map()) !!! ERROR !!!
+  //   console.log(hobby.map()) !!! ERROR !!! because of type
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("I am an Author");
 }
