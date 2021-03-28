2714ec10ebdc3cb7071d297fdad72d0a7a3f950a
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-28 10:21:53 +0200

    this keyword, public, private, readonly

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 class Department {
-  name: string;
+  // private id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id
+    // this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Luis");
-console.log(accounting);
+const accounting = new Department("ID1", "Luis Schekerka Consulting");
+
+accounting.addEmployee("Luis");
+accounting.addEmployee("Louise");
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "Test", describe: accounting.describe };
+
+// accountingCopy.describe();
