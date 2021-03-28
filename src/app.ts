98446d3f4f75952b5e705634a1924b95a139ec26
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-28 23:04:01 +0200

    read only on interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 interface Greetable {
-  name: string;
+  readonly name: string;
 
   greet(phrase: string): void;
 }
@@ -19,7 +19,7 @@ class Person implements Greetable {
 }
 
 let user1: Greetable;
-
 user1 = new Person("Luis", 26);
+// user1.name = "Louise"; !!!ERROR!!! Because of readonly
 console.log(user1);
 user1.greet("Hi there - I'm");
