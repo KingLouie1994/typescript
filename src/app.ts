0ff25bb7a364a290a64c4006c1a847c0321eac1a
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-29 17:14:07 +0200

    function overloads

--- src/app.ts
+++ src/app.ts
@@ -21,12 +21,16 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
-const add = (a: Combinable, b: Combinable) => {
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
+function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
-};
+}
 
 type UnknownEmployee = Employee | Admin;
 
@@ -110,4 +114,5 @@ interface ErrorContainer {
 
 const errorBag: ErrorContainer = {
   email: "Not a valid email address!",
+  username: "Username already taken!",
 };
