c617fb5af0a03e15f31f925c898aca4c3625c2c9
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-29 16:21:49 +0200

    type guards

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "Luis",
-  privileges: ["read & write"],
+  privileges: ["read", "write"],
   startDate: new Date(),
 };
 
@@ -20,3 +20,55 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+const add = (a: Combinable, b: Combinable) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+};
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+  console.log(`Name: ${emp.name}`);
+  if ("privileges" in emp) {
+    emp.privileges.map((priv) => console.log(`Privileg: ${priv}`));
+  }
+  if ("startDate" in emp) {
+    console.log(`Start Date: ${emp.startDate}`);
+  }
+};
+
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: "Luis", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log(`Loading cargo: ${amount}kg`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+};
+
+useVehicle(v1);
+useVehicle(v2);
