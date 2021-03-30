c11fdbb13c261e642fe0889e1ccdbbf8db7ed1a2
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-30 11:38:43 +0200

    partial and readonly

--- src/app.ts
+++ src/app.ts
@@ -77,3 +77,24 @@ console.log(textStorage.getItems());
 // objStorage.addItem({ name: "Louise" });
 // objStorage.removeItem({ name: "Luis" });
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Luis", "Louise"];
+// names.push("Emily"); not allowed because of Readonly
