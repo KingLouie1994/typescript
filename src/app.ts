c0394849a357cbbd432ed7903a03b437a5f2b399
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-30 11:15:02 +0200

    keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,32 @@ const mergedObj = merge(
 );
 console.log(mergedObj);
 
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = "Got no value.";
+  if (element.length === 1) {
+    description = `Got 1 element`;
+  } else if (element.length > 1) {
+    description = `Got ${element.length} elements`;
+  }
+  return [element, description];
+}
+
+console.log(countAndDescribe("Hello World!"));
+console.log(countAndDescribe(["Golf", "Coding"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value: ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: "Luis" }, "name"));
+
 // const names: Array<string> = ["Luis", "Louise"];
 // // names[0].split(" ");
 
