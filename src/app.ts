f7f8d4a21fc86a132468489ea2f61107e1a697f0
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-30 11:28:14 +0200

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// const names: Array<string> = ["Luis", "Louise"];
+// // names[0].split(" ");
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split("");
+// });
+
 function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
@@ -34,15 +47,33 @@ function extractAndConvert<T extends object, U extends keyof T>(
 
 console.log(extractAndConvert({ name: "Luis" }, "name"));
 
-// const names: Array<string> = ["Luis", "Louise"];
-// // names[0].split(" ");
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
 
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("This is done!");
-//   }, 2000);
-// });
+  addItem(item: T) {
+    this.data.push(item);
+  }
 
-// promise.then((data) => {
-//   data.split("");
-// });
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Luis");
+textStorage.addItem("Louise");
+textStorage.removeItem("Luis");
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Luis" });
+// objStorage.addItem({ name: "Louise" });
+// objStorage.removeItem({ name: "Luis" });
+// console.log(objStorage.getItems());
