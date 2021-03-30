41acabceb35cf5096494add8dfc95d73700ce381
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-03-30 13:52:08 +0200

    Validation with decorators

--- src/app.ts
+++ src/app.ts
@@ -115,6 +115,74 @@ class Printer {
 }
 
 const p = new Printer();
-
 const button = document.querySelector("button")!;
 button.addEventListener("click", p.showMessage);
+
+// ---
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[];
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Require(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    [propName]: ["required"],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    [propName]: ["positive"],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  for (const prop in objValidatorConfig) {
+    for (const validator in objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          return !!obj[prop];
+        case "positive":
+          return obj[prop] > 0;
+      }
+    }
+  }
+  return true;
+}
+
+class Course {
+  @Require
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    (this.title = t), (this.price = p);
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert("Invalid input - please try again");
+    return;
+  }
+
+  console.log(createdCourse);
+});
